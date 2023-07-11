9538e897d3e41ffd0202bd5ca2263f585bd28dec
wubiao <wubiao@mail.51vv.com>
2023-07-11 15:55:04 +0800

    feat: 修改成 ggml-vic7b-q5_1

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@
 cp example.env .env
 
 mkdir -p models
-wget -c https://gpt4all.io/models/ggml-gpt4all-j-v1.3-groovy.bin -O models/ggml-gpt4all-j-v1.3-groovy.bin
+wget -c https://huggingface.co/vicuna/ggml-vicuna-7b-1.1/resolve/main/ggml-vic7b-q5_1.bin -O models/ggml-vic7b-q5_1.bin
 
